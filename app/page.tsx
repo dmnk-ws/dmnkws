dc4306f69ca4f35233d2afd3b71989b17b8c3283
Dominik Wies <domi.wies@googlemail.com>
2025-05-30 12:44:00 +0200

    Adjust min screen height

--- app/page.tsx
+++ app/page.tsx
@@ -8,20 +8,32 @@ export default function Home() {
       <header>
         <Nav />
       </header>
-      <main className="mt-19 md:mt-25">
-        <section id="home" className="min-h-screen bg-red-100 p-8">
+      <main className="mt-18 md:mt-24 xl:mt-25">
+        <section
+          id="home"
+          className="min-h-[calc(100vh-72px)] md:min-h-[calc(100vh-96px)] xl:min-h-[calc(100vh-100px)] bg-red-100 p-8"
+        >
           <h1 className="text-4xl mb-4">Home</h1>
           <p>This is the Home section.</p>
         </section>
-        <section id="about" className="min-h-screen bg-blue-100 p-8">
+        <section
+          id="about"
+          className="min-h-[calc(100vh-72px)] md:min-h-[calc(100vh-96px)] xl:min-h-[calc(100vh-100px)] bg-blue-100 p-8"
+        >
           <h1 className="text-4xl mb-4">About</h1>
           <p>This is the About section.</p>
         </section>
-        <section id="portfolio" className="min-h-screen bg-green-100 p-8">
+        <section
+          id="portfolio"
+          className="min-h-[calc(100vh-72px)] md:min-h-[calc(100vh-96px)] xl:min-h-[calc(100vh-100px)] bg-green-100 p-8"
+        >
           <h1 className="text-4xl mb-4">Portfolio</h1>
           <p>This is the Portfolio section.</p>
         </section>
-        <section id="contact" className="min-h-screen bg-yellow-100 p-8">
+        <section
+          id="contact"
+          className="min-h-[calc(100vh-72px)] md:min-h-[calc(100vh-96px)] xl:min-h-[calc(100vh-100px)] bg-yellow-100 p-8"
+        >
           <h1 className="text-4xl mb-4">Contact</h1>
           <p>This is the Contact section.</p>
         </section>
