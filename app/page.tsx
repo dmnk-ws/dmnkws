19800ac989abc69f2fde0e3d7d9fdc09a7f01052
Dominik Wies <domi.wies@googlemail.com>
2025-06-19 18:17:56 +0200

    Add main component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import Nav from '@/components/navigation/nav';
-import { ActiveSectionProvider } from '@/context/ActiveSectionProvider';
+import Nav from '@/components/navigation/Nav';
 import Footer from '@/components/footer/Footer';
+import Main from '@/components/section/Main';
+import { ActiveSectionProvider } from '@/context/ActiveSectionProvider';
 
 export default function Home() {
   return (
@@ -8,31 +9,25 @@ export default function Home() {
       <header>
         <Nav />
       </header>
-      <main className="mt-18 md:mt-24 xl:mt-25">
-        <section
-          id="home"
-          className="min-h-[calc(100vh-72px)] md:min-h-[calc(100vh-96px)] xl:min-h-[calc(100vh-100px)] bg-red-100 p-8"
-        >
-          <h1 className="text-4xl mb-4">Home</h1>
-          <p>This is the Home section.</p>
-        </section>
+      <main className="mx-14 md:mx-25 2xl:mx-auto max-w-screen-xl mt-18 md:mt-24 xl:mt-25">
+        <Main />
         <section
           id="about"
-          className="min-h-[calc(100vh-72px)] md:min-h-[calc(100vh-96px)] xl:min-h-[calc(100vh-100px)] bg-blue-100 p-8"
+          className="min-h-[calc(100vh-72px)] md:min-h-[calc(100vh-96px)] xl:min-h-[calc(100vh-100px)] border border-gray-300"
         >
           <h1 className="text-4xl mb-4">About</h1>
           <p>This is the About section.</p>
         </section>
         <section
           id="portfolio"
-          className="min-h-[calc(100vh-72px)] md:min-h-[calc(100vh-96px)] xl:min-h-[calc(100vh-100px)] bg-green-100 p-8"
+          className="min-h-[calc(100vh-72px)] md:min-h-[calc(100vh-96px)] xl:min-h-[calc(100vh-100px)] border border-gray-300"
         >
           <h1 className="text-4xl mb-4">Portfolio</h1>
           <p>This is the Portfolio section.</p>
         </section>
         <section
           id="contact"
-          className="min-h-[calc(100vh-72px)] md:min-h-[calc(100vh-96px)] xl:min-h-[calc(100vh-100px)] bg-yellow-100 p-8"
+          className="min-h-[calc(100vh-72px)] md:min-h-[calc(100vh-96px)] xl:min-h-[calc(100vh-100px)] border border-gray-300"
         >
           <h1 className="text-4xl mb-4">Contact</h1>
           <p>This is the Contact section.</p>
---
+++ components/section/Main.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { Typewriter } from 'react-simple-typewriter';
+import Image from 'next/image';
+import laptop from '../../assets/images/laptop.png';
+
+export default function Main() {
+  const introText = "Hey! I'm Dominik, a ";
+
+  return (
+    <section
+      id="home"
+      className="grid grid-cols-1 xl:grid-cols-2 gap-2 h-[calc(100vh-72px)] md:h-[calc(100vh-96px)] xl:h-[calc(100vh-100px)]"
+    >
+      <div className="flex flex-col h-auto xl:h-full mt-20 xl:mt-55">
+        <h1 className="text-3xl md:text-6xl font-bold mb-4">{introText}</h1>
+        <h1 className="text-3xl md:text-6xl mb-4 font-bold bg-gradient-to-r from-indigo-600 to-pink-200 bg-clip-text text-transparent">
+          <Typewriter
+            loop={0}
+            words={[
+              'React Developer',
+              'PHP Enthusiast',
+              'Symfony Lover',
+              'Type-safe Writer',
+            ]}
+            cursor
+            typeSpeed={250}
+            deleteSpeed={125}
+            delaySpeed={1000}
+          />
+        </h1>
+        <p className="py-4">
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+          eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+          voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
+          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        </p>
+      </div>
+      <div className="flex justify-center h-auto xl:h-full">
+        <div className="flex justify-center xl:items-center">
+          <Image
+            src={laptop}
+            alt="Laptop image"
+            className="w-auto h-75 md:h-125 xl:w-full xl:h-auto"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
