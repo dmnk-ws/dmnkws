72b0f050d47f2476b807b1ee29e6697791811d6b
Dominik Wies <domi.wies@googlemail.com>
2025-09-21 16:54:18 +0200

    Add contact form

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Nav from '@/components/navigation/Nav';
 import Footer from '@/components/footer/Footer';
 import Main from '@/components/section/Main';
+import Contact from '@/components/section/Contact';
 import { ActiveSectionProvider } from '@/context/ActiveSectionProvider';
 
 export default function Home() {
@@ -25,13 +26,7 @@ export default function Home() {
           <h1 className="text-4xl mb-4">Portfolio</h1>
           <p>This is the Portfolio section.</p>
         </section>
-        <section
-          id="contact"
-          className="min-h-[calc(100vh-72px)] md:min-h-[calc(100vh-96px)] xl:min-h-[calc(100vh-100px)] border border-gray-300"
-        >
-          <h1 className="text-4xl mb-4">Contact</h1>
-          <p>This is the Contact section.</p>
-        </section>
+        <Contact />
       </main>
       <Footer />
     </ActiveSectionProvider>
---
+++ components/form/Textarea.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+interface TextareaProps {
+  label: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  name: string;
+  placeholder?: string;
+}
+
+export default function Textarea({
+  label,
+  value,
+  onChange,
+  name,
+  placeholder,
+}: TextareaProps) {
+  return (
+    <div>
+      <label htmlFor={name} className="block text-sm font-medium text-gray-300 mb-2">
+        {label}
+      </label>
+      <textarea
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        required
+        rows={6}
+        className="w-full px-3 py-2 border-b-2 border-gray-300 shadow-sm focus:outline-none focus:border-b-2 focus:border-b-blue-500"
+        placeholder={placeholder ?? label}
+      />
+    </div>
+  );
+}
